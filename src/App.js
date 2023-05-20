3be7049a25b29fce65691e238a725a8b17c5f682
huzaifa273 <malikhuzaifaawais1@gmail.com>
2023-05-20 16:26:30 +0500

    working

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ca4ef4d027c43ef4ec5e6f0d37dee156`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
